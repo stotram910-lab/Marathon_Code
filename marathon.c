f244fb573017493e3686a3faa9f3fbfddb6b71f8
sTOTraM750 <sparshranjan750@gmail.com>
2026-08-02 12:35:43 +0530

    Initial commit: Marathon distance calculator

---
+++ marathon.c
@@ -0,0 +1,13 @@
+/* The distance of the marathon in kilometers */
+
+#include <stdio.h>
+
+int main(void)
+{
+    int miles = 26, yards = 385;
+    double kilometers;
+
+    kilometers = 1.609 * (miles + yards / 1760.0);
+    printf("\nA marathon is %f kilometers.\n\n", kilometers);
+    return 0;
+}
